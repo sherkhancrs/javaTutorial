f470375dc1f4534edbf06605c479a607a158a0c3
Sherkhan Zhanibekov <szhanibekov@almanit.kz>
2020-06-09 18:33:55 +0600

    refactored variables

--- src/com/sherkhancrs/MortgageCalculator.java
+++ src/com/sherkhancrs/MortgageCalculator.java
@@ -6,15 +6,22 @@ import java.util.Scanner;
 
 public class MortgageCalculator {
     public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
         System.out.print("Principal: ");
         int principal = scanner.nextInt();
+
         System.out.print("Annual Interest Rate: ");
         double rate = scanner.nextDouble();
+
         System.out.print("Period (Years): ");
         int years  = scanner.nextInt();
-        double monthlyRate = rate/100/12;
-        int numberOfPayments = years*12;
+
+        double monthlyRate = rate/PERCENT/MONTHS_IN_YEAR;
+        int numberOfPayments = years*MONTHS_IN_YEAR;
         double mortgage = (principal * monthlyRate*Math.pow(1+monthlyRate,numberOfPayments))/(Math.pow((1+monthlyRate),numberOfPayments)-1);
         System.out.println("Mortgage: "+NumberFormat.getCurrencyInstance().format(mortgage));
     }
